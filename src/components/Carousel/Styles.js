3cacaa5126e403f4ce88e30e71101576a9098da1
onejuneee <onejuneee721@gmail.com>
2023-09-03 21:42:55 +0900

    carousel 컴포넌트 생성

---
+++ src/components/Carousel/Styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+`;
+
+export const CarouselContainer = styled.div`
+  margin-top: 20px;
+`;
