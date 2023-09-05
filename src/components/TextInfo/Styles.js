7f6342a8c337a81dc615190c51c5d34ce89f11fd
onejuneee <onejuneee721@gmail.com>
2023-09-05 16:59:11 +0900

    Feat: 해시태그 컴포넌트 구현

--- src/components/TextInfo/Styles.js
+++ src/components/TextInfo/Styles.js
@@ -34,15 +34,21 @@ export const SubTitleContent = styled.h6`
 export const SkillLabel = styled.h6`
   color: #333;
   margin-top: 2rem;
+  margin-bottom: 0;
   font-weight: 600;
   font-size: 1.6rem;
   line-height: 1.75;
-  margin-block-start: 2.33em;
-  margin-block-end: 2.33em;
-  margin-inline-start: 0;
-  margin-inline-end: 0;
   font-family: Pretendard;
 `;
+
+export const SkillBox = styled.div`
+  display: flex;
+  margin-top: 0.5rem;
+`;
+
+// grid-gap: 10px 8px;
+// gap: 10px 8px;
+
 
 
 
--- src/components/TextInfo/TextInfo.jsx
+++ src/components/TextInfo/TextInfo.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import * as T from './Styles';
+import HashTag from "../HashTag/HashTag";
 
 
 
 
 function TextInfo() {
+
+  const text = ['Confluence', 'JIRA', 'Figma'];
   return (
     <T.TextContainer>
       <T.TextContent>조직 소개</T.TextContent>
@@ -18,6 +21,10 @@ function TextInfo() {
       <T.SubTitleContent>혜택 및 복지</T.SubTitleContent>
       <T.TextContent>내용이 들어갑니다.</T.TextContent>
       <T.SkillLabel>기술스택 ・ 툴</T.SkillLabel>
+      <T.SkillBox>
+        {text.map((label, index) => (
+          <HashTag key={index} text={label} color="#E4F4EC" />
+        ))}</T.SkillBox>
     </T.TextContainer>
 
   );
